57afd1c3c66c3b8ca7480b5d896db85b925c695a
overplex-eth <112352174+overplex-eth@users.noreply.github.com>
2022-09-05 17:49:11 +0200

    Update modal.py

--- examples/modal.py
+++ examples/modal.py
@@ -1,26 +1,22 @@
+import disnake
+from disnake.ext import commands
 
-
- @commands.slash_command(name="profilo", description="crea il tuo profilo", guild_ids=[865717588684570634])
+ @commands.slash_command(name="<slash command name>", description="<slash command description>") #change the name + description
     async def test_command(self, interaction: disnake.ApplicationCommandInteraction):
-
-        embed = disnake.Embed(title="Crea il tuo profilo", description="Crea un tuo profilo con il nostro bot")
-
-        await interaction.response.send_message(embed=embed, ephemeral=True)
-
 
 class MyModal(disnake.ui.Modal):
     def __init__(self) -> None:
         components = [
-            disnake.ui.TextInput(
-                label="Nome",
-                placeholder="il tuo nome social",
-                custom_id="nome",
+            disnake.ui.TextInput( # imput thext for the modal
+                label="<name>",
+                placeholder="<under the text (uneditable text)>",
+                custom_id="<id for the component>",
                 style=disnake.TextInputStyle.short,
-                max_length=50,
+                max_length=50, # max lenght is for see how much text they can write max (you can use min_lenght for put a minimum necessary text)
             ),
             disnake.ui.TextInput(
-                label="Descrizione",
-                placeholder="Descrivi il tuo canale",
+                label="<name>",
+                placeholder="<under the text (uneditable text)>",
                 custom_id="descrizione",
                 style=disnake.TextInputStyle.short,
                 min_length=5,
@@ -28,13 +24,13 @@ class MyModal(disnake.ui.Modal):
             ),
         ]
         super().__init__(
-            title="Profilo", custom_id="profile_tag", components=components
+            title="<title for the footer>", custom_id="<id>", components=components # to send the components you need to say it with components=components
         )
 
     async def callback(self, inter: disnake.ModalInteraction) -> None:
-        embed = disnake.Embed(title="Profilo")
+        embed = disnake.Embed(title="<name of the callback embed>") # name the callback
         for key, value in inter.text_values.items():
-            embed.add_field(name=key.capitalize(), value=value, inline=False)
+            embed.add_field(name=key.capitalize(), value=value, inline=False) # put here ephemeral=True for put "only see from you" (like line 37)
         await inter.response.send_message(embed=embed)
 
     async def on_error(self, error: Exception, inter: disnake.ModalInteraction) -> None:
