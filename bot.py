2d3adddbae6602a2bb10601013ba6f61c31224f4
overplex-eth <112352174+overplex-eth@users.noreply.github.com>
2022-09-05 20:56:43 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,3 @@
-""""
-Copyright © Krypton 2022 - https://github.com/kkrypt0nn (https://krypton.ninja)
-Description:
-This is a template to create your own discord bot in python.
-
-Version: 4.1.1
-"""
-
 import json
 import os
 import platform
