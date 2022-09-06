5e42095cb6760b1ffacb71da6ba4a41bef6c661d
BorgoSupremo69 <112983228+BorgoSupremo69@users.noreply.github.com>
2022-09-06 22:38:30 +0200

    Create install.bat
    Install the bot libraries

---
+++ install.bat
@@ -0,0 +1,6 @@
+echo installing requirements to use the bot
+sleep 2
+pip install -r requirements.txt
+sleep 2 
+echo python bot.py >> start.bat
+echo now open the file "start.bat" when u loaded "config.json"
